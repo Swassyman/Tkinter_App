7d5de49c564bddc0662ac4ab62fe21bd85020d92
Swassy <swassyman2005@gmail.com>
2023-11-07 15:48:10 +0530

    calc_app.py works now!

--- Apps/clock_app.py
+++ Apps/clock_app.py
@@ -5,7 +5,6 @@ class clock_app:
     def __init__(self, root):
         self.root = root
         self.root.title("Clock")
-        root.destroy()
         # Create widgets for the second clock
         # from here
         def update():
@@ -16,19 +15,18 @@ class clock_app:
             day_lab.config(text=day_str)
             date_str = strftime("%d :%B:%Y")
             date_lab.config(text=date_str)
-        clock = Tk()
-        clock.title("Clock")
-        clock.resizable(False,False)
-        photo = PhotoImage(master=clock, file="C:/Code/Tkinter_App/Assets/clock.png")
-        clock.iconphoto(False,photo)
-        time_lab=Label(clock,font=("yellow",50),bg="yellow")
+        root.title("Clock")
+        root.resizable(False,False)
+        photo = PhotoImage(master=root, file="C:/Code/Tkinter_App/Assets/clock.png")
+        root.iconphoto(False,photo)
+        time_lab=Label(root,font=("yellow",50),bg="yellow")
         time_lab.pack()
-        day_lab=Label(clock,font=("arial",40))
+        day_lab=Label(root,font=("arial",40))
         day_lab.pack()
-        date_lab=Label(clock,font=("arial",40),bg="yellow")
+        date_lab=Label(root,font=("arial",40),bg="yellow")
         date_lab.pack()
         update()
-        clock.mainloop()
+        root.mainloop()
 
        
 
